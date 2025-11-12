bf68d680caa4817aa5f7c44fd021387ad3ebb348
AmdRhn <amdrhn0@gmail.com>
2025-11-12 14:53:01 +0500

    changes by client

--- common.js
+++
@@ -1,8 +0,0 @@
-async function loadHTML(id, file) {
-  const el = document.getElementById(id);
-  const res = await fetch(file);
-  el.innerHTML = await res.text();
-}
-
-
-loadHTML("footer", "footer.html");
